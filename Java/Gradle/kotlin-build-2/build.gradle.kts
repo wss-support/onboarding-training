4b2844c06584f379525ea2b0824ae6e221f54946
katerinaozerova <53837535+katerinaozerova@users.noreply.github.com>
2021-06-12 15:31:52 -0400

    Create build.gradle.kts

---
+++ Java/Gradle/kotlin-build-2/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `java-library`
+}
+dependencies {                              
+    api("junit:junit:4.13")
+    implementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13")
+}
+configurations {                            
+    implementation {
+        resolutionStrategy.failOnVersionConflict()
+    }
+}
+sourceSets {                                
+    main {                                  
+        java.srcDir("src/core/java")
+    }
+}
+java {                                      
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+tasks {
+    test {                                  
+        testLogging.showExceptions = true
+    }
+}
